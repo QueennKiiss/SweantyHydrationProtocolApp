35b7893a7869ffc54ff9f6afec300da073e49b1a
mmaestro <mmaestroizquierdo@gmail.com>
2023-05-15 14:11:52 +0200

    refactor: modify blueprint name

--- hydration/hydration_procotol.py
+++ hydration/hydration_procotol.py
@@ -7,9 +7,9 @@ from flask import Blueprint
 from flask import render_template
 
 
-bp = Blueprint("hydration_protocol", __name__, static_folder='static', template_folder='templates')
+bp_hydration = Blueprint("hydration_protocol", __name__, static_folder='static', template_folder='templates')
 
 
-@bp.route("/", methods=['GET', 'POST'])
+@bp_hydration.route("/", methods=['GET', 'POST'])
 def hydration_protocol_app():
     return render_template("hydration_protocol.html")
--- login/login.py
+++ login/login.py
@@ -7,20 +7,27 @@ from flask import Blueprint
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import session
 
 
-bp = Blueprint("login", __name__, static_folder='static', template_folder='templates')
+bp_login = Blueprint("login", __name__, static_folder='static', template_folder='templates')
 
 
-@bp.route("/", methods=['POST', 'GET'])
-@bp.route("/login", methods=['POST', 'GET'])
+@bp_login.route("/", methods=['POST', 'GET'])
 def login():
+    # if 'username' in session:
+    #     return f'Logged in as {session["username"]}'
     if request.method == 'POST':
-        print(f"POST: {request.method}")
-        user = request.form["username"]
+        # session['username'] = request.form["username"]
         # relative path to the method. If the method is in another module, use
         # "blueprint_name.method_name" to call required path
         return redirect(url_for("hydration_protocol.hydration_protocol_app"))
     else:
-        print(f"GET: {request.method}")
         return render_template("login.html")
+
+
+# @bp_login.route('/logout')
+# def logout():
+#     # remove the username from the session if it's there
+#     session.pop('username', None)
+#     return redirect(url_for('login'))
