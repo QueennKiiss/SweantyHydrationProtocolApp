7874886ea7741d5924db4304194c35b8a497e2fe
mmaestro <mmaestroizquierdo@gmail.com>
2023-07-27 17:16:24 +0200

    feat: modify local  db manager
    To improve the suplement item filtering such a module is modified.

--- database/ddbb_local_manager.py
+++ database/ddbb_local_manager.py
@@ -11,24 +11,39 @@ class LocalDBManager:
     """
 
     def __init__(self):
-        pass
+        self.db_content = None
 
-    def get_all_content(self, table: str) -> pd.DataFrame:
+    def _format_db_entry(self, entry: pd.Series) -> str:
+        suplement_type = entry['TYPE']
+        suplement_brand = entry['BRAND']
+        suplement_model = entry['MODEL']
+        suplement_salt_content = entry['(mg_NaCl) per capsula/gel/1L']
+        return f"{suplement_type} {suplement_brand} {suplement_model} -->  {suplement_salt_content} (mg_NaCl) per capsula/gel/1L"
+
+    def read_data_from_db(self, table: str) -> pd.DataFrame:
         """
         Get all the information stored in a database table of the database
         """
         content = pd.read_csv(table)
-        print(content)
-        print(content.dtypes)
-        print(content.attrs)
+        self.db_content = content
         return content
 
-    def get_filtered_content(self, table: str, **filters) -> list:
+    def get_all_db_content(self) -> list:
         """
         Get specified information from the database using filters or indicators
         """
-        content = pd.read_csv(table)
-        # print(content)
+        supplement_list = []
+        for _, supplement in self.db_content.iterrows():
+            formatted_supplement = self._format_db_entry(supplement)
+            supplement_list.append(f"{formatted_supplement}")
+
+        return supplement_list
+
+    def get_filtered_content(self, **filters) -> list:
+        """
+        Get specified information from the database using filters or indicators
+        """
+        content = self.db_content
         # To filter by row or columns names
         # filtered_content = content.filter(like='BRAND', axis=1)
         if not filters:
@@ -38,20 +53,13 @@ class LocalDBManager:
 
         for key, value in filters.items():
             filtered_content = content[content[key] == value]
-            print(f"FILTERED: {filtered_content}")
+            # print(f"FILTERED: {filtered_content}")
             # json_filtered_content = filtered_content.to_json(orient="records")
             # print(f"JSONed: {json_filtered_content}")
 
-        suplement_list = []
-        for suplement in filtered_content.iterrows():
-            suplement_list.append(f"{suplement}")
-        print(suplement_list)
+        supplement_list = []
+        for _, supplement in filtered_content.iterrows():
+            formatted_supplement = self._format_db_entry(supplement)
+            supplement_list.append(f"{formatted_supplement}")
 
-        return suplement_list
-
-
-if __name__ == '__main__':
-    db_manager = LocalDBManager()
-    # db_manager.get_all_content('suplement_db.csv')
-    db_manager.get_filtered_content('suplement_db.csv', TYPE='Beguda')
-    # db_manager.get_filtered_content('suplement_db.csv')
+        return supplement_list
